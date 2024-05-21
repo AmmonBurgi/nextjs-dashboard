68fd1cb7c6b7050418245c841f336ddf66ebca00
Ammon Burgi <ammon@indicio.tech>
2024-05-20 23:01:03 -0600

    feat: revenue and invoices

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Im the customers page!</p>
+  return <p>Im the customers page!</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Im the invoices page!</p>
+  return <p>Im the invoices page!</p>;
 }
